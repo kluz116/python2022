6df07536ff96e9c511c52d6c6e1980d7701435d5
kluz116 <engallanmusembya@gmail.com>
2022-01-12 12:12:09 +0300

    Python lists

---
+++ lists.py
@@ -0,0 +1,14 @@
+listOne  =  ['a', 'b', 'c', 'd']
+listTwo =  ['e', 'f', 'g']
+
+#Index() Method which finds the index of an element in the list
+listOne.index('b')#1
+listTwo.index('g')#2
+
+#Append() Methhod which adda an element to the end of the list
+
+currencies = ['Dollar', 'Euro', 'Pound']
+currencies.append('Yen')
+currencies.append('UGX')
+
+print(currencies)
---
+++ pandas.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#create DataFrame
+df = pd.DataFrame({'points': [25, 12, 15, 14],
+                   'assists': [5, 7, 13, 12]})
