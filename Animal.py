d0ae792d996016b1e5d4a36ceb8c0e77d4364c68
Allan Williams Musembya Kluz <engallanmusembya@gmail.com>
2022-03-18 16:27:04 +0300

    oop

--- Animal.py
+++ Animal.py
@@ -10,8 +10,13 @@ class Animal(ABC):
 class Lion(Animal):
 
     def feed(self):
-        print('Feeds on raw meet..')
+        print('Feeds on raw meet..........')
 
+class Fish(Animal):
+    def feed(self):
+        print('Feeds on sea seeds.........')
 
-obj = Lion()
-obj.feed()
+if __name__=='__main__':
+    zoo = [Lion(),Fish()]
+    for animal in zoo:
+        animal.feed()
